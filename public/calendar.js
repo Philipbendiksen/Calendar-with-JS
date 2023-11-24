265c75e711017911e5e518678c450d087f0c8b30
Philip Bendiksen <philipbendiksen@gmail.com>
2023-11-24 15:54:56 +0100

    Made calender dynamic

--- public/calendar.js
+++ public/calendar.js
@@ -1,31 +1,50 @@
-window.addEventListener('DOMContentLoaded', main);
-
-function main() {
-    console.log('Hiloow fellow!');
-}
-
-
-
-
 let nav = 0;
 let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
-const calender = document.getElementById('calender');
+const calender = document.getElementById('calenderNumber');
 const weekdays = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'];
 
 function load() {
     const dt = new Date();
 
-    console.log(dt);
-
     const day = dt.getDate();
     const month = dt.getMonth();
     const year = dt.getFullYear();
 
+
+    const firstDayOfMonth = new Date(year, month, 1);
     const daysInMonth = new Date(year, month + 1, 0).getDate();
 
+    const dateString = firstDayOfMonth.toDateString('sv-eu', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+    });
+
+    const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);   /* Loggar - 1 ska logga 5  */
+
+    for (let i = 1; i <= paddingDays + daysInMonth; i++) {
+        const daySquare = document.createElement('div');
+        daySquare.classList.add('day');
+
+        if (i > paddingDays) {
+            daySquare.innerText = i - paddingDays;
+
+            daySquare.addEventListener('click', () => console.log('click'));
+
+        } else {
+            daySquare.classList.add('padding');
+
+        }
+
+        calender.appendChild(daySquare);
+
+    }
+
 }
+
 
 load();
 
@@ -33,9 +52,5 @@ load();
 
 
 
-
-
-
-
-/* 13:30 i videon  */
+/* Kalendern börjar inte på nr 1, kolla upp varför det inte fungerar och var 1an tagit vägen  */
 
