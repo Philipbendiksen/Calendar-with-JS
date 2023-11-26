3a3a745cc8a998e417ee6325b87ee6d4e8824b94
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2023-11-26 22:10:38 +0100

    add divs, day, time and date

--- public/welcome.js
+++ public/welcome.js
@@ -1,17 +1,61 @@
 function createWelcomeSegment() {
-    const header = document.createElement('header');
- 
-    document.body.prepend(header);
+  const header = document.createElement("header");
 
-    const titleDiv = document.createElement("div");
-    titleDiv.className = "title";
-    titleDiv.innerHTML = "SEPO kalender";
-    
-    header.appendChild(titleDiv); 
+  document.body.prepend(header);
 
-    const iconDiv = document.createElement("div");
-    iconDiv.className = "seasonalIcon";
-    iconDiv.textContent = "⛄️❄️";
+  const titleDiv = document.createElement("div");
+  titleDiv.className = "title";
+  titleDiv.innerHTML = "SEPO kalender";
 
-    header.appendChild(iconDiv);
+  header.appendChild(titleDiv);
+
+  const iconDiv = document.createElement("div");
+  iconDiv.className = "seasonalIcon";
+  iconDiv.textContent = "⛄️❄️";
+
+  header.appendChild(iconDiv);
+
+  const welcomeMobile = document.createElement("div");
+  welcomeMobile.className = "welcomeSegment-mobile";
+  welcomeMobile.setAttribute("data-cy", "welcome-segment");
+
+  header.appendChild(welcomeMobile);
+
+  const welcomeDayDiv = document.createElement("div");
+  welcomeDayDiv.className = "welcomeDay";
+
+  const dayNames = [
+    "Monday",
+    "Tuesday",
+    "Wedensday",
+    "Thursday",
+    "Friday",
+    "Saterday",
+    "Sunday",
+  ];
+  const now = new Date();
+  const day = dayNames[now.getDay()];
+  welcomeDayDiv.textContent = day;
+
+  header.appendChild(welcomeDayDiv);
+
+  const welcomeTimeDiv = document.createElement("div");
+  welcomeTimeDiv.className = "welcomeTime";
+
+  const hour = now.getHours();
+  const minutes = now.getMinutes();
+  const timeString = hour + ":" + (minutes < 10 ? "0" : "") + minutes;
+  welcomeTimeDiv.textContent = timeString;
+  header.appendChild(welcomeTimeDiv);
+
+  const welcomeDateDiv = document.createElement("div");
+  welcomeDateDiv.className = "welcomeDate";
+
+  const date = now.getDate();
+  const month = now.getMonth();
+  const year = now.getFullYear();
+  const dateString = year + "-" + month + "-" + date;
+  welcomeDateDiv.textContent = dateString;
+
+  header.appendChild(welcomeDateDiv);
 }
