a7f5f5db968d5661d5aebf0591c462b15047a761
oscar.jigbring <oscar.jigbring@hotmail.se>
2023-11-28 14:52:42 +0100

    added more to confirm modal

--- public/deleteModal.js
+++
@@ -1,5 +0,0 @@
-function confirmationModal() {
-  const modalBackdrop = document.querySelector("#modalBackdrop");
-  const notificationModalBtn1 = document.querySelector("#notiModalBtn1");
-  const notificationModalBtn2 = document.querySelector("#notiModalBtn2");
-}
