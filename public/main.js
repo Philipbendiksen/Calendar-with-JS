e58a714fe56dbc077a8684000c20b12afe3c663d
Eleni001 <Eleni.SolomonSamuelsson@medieinstitutet.se>
2023-11-29 14:11:25 +0100

    add init welcome segment

--- public/main.js
+++ public/main.js
@@ -3,9 +3,7 @@ window.addEventListener("DOMContentLoaded", main);
 function main() {
   PopUp();
   // confirmationModal("testtitel", "testfråga", "ja", "nej");
-  updateActiveDate(todayString());
   createCommonHeader();
-  document.querySelector("header").appendChild(createWelcomeSegment("welcomeSegment-mobile"));
-  document.querySelector("aside").prepend(createWelcomeSegment("welcomeSegment-desktop"));
+  initWelcomeSegment();
 }
 
