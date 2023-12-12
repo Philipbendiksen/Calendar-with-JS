9f2b13c74886509ccd69dc2949d5c45341ba64ce
sallymaria99 <sallymaria@live.se>
2023-12-12 08:57:38 +0100

    add logo, add header in HTML instead

--- public/scripts/welcome.js
+++ public/scripts/welcome.js
@@ -1,5 +1,5 @@
 function initWelcomeSegment() {
-  createCommonHeader();
+  //createCommonHeader();
 
   document
     .querySelector("header")
@@ -9,22 +9,16 @@ function initWelcomeSegment() {
     .prepend(createWelcomeSegment("welcomeSegment-desktop"));
 }
 
-function createCommonHeader() {
+/*function createCommonHeader() {
   const header = document.createElement("header");
   document.body.prepend(header);
 
-  const image = document.createElement("img");
-  image.setAttribute("src", "public/img/logga.png");
-  image.setAttribute("alt", "logotyp för kalender");
-
-  document.body.appendChild(image);
-
-  const titleDiv = document.createElement("div");
+  /*const titleDiv = document.createElement("div");
   titleDiv.className = "title";
   titleDiv.innerHTML = "SEPO kalender";
   header.appendChild(titleDiv);
 }
-
+*/
 function createWelcomeSegment(welcomeSegmentClassName) {
   const welcomeSegment = document.createElement("div");
   welcomeSegment.className = welcomeSegmentClassName;
